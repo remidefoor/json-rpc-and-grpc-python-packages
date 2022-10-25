0d9b51934bf7f9a9473be424c89b732d4ad26b22
Rémi Defoor <remi.defoor@student.howest.be>
2022-10-25 18:15:07 +0200

    create JSON-RPC POC

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+from enum import Enum
+
 from flask import Flask
+from jsonrpc.backend.flask import api
 
 app = Flask(__name__)
+app.add_url_rule("/", "api", api.as_view(), methods=["POST"])
+
+GAS_FEE: int = 5
 
 
-@app.route("/")
-def hello_world() -> str:
-    """Return hello world."""
-    return "<p>Hello, World!</p>"
+class PackageSize(Enum):
+    LETTER = 1
+    SMALL = 2
+    MEDIUM = 3
+    LARGE = 4
+
+
+@api.dispatcher.add_method
+def calculate_fee(package_size: str):
+    formatted_package_size = package_size.upper()
+    try:
+        return GAS_FEE * PackageSize[formatted_package_size].value
+    except:
+        return 0
